f42b1c82f6295fea0ac9a5a078ab71f6c60d797a
Caio Rosa <126679544+caiorosa-seenons@users.noreply.github.com>
2023-10-26 10:39:33 +0200

    Update src/services/availability/service_provider_availability.service.ts
    Co-authored-by: Gazy Mahomar <gmahomarf@users.noreply.github.com>

--- src/services/availability/service_provider_availability.service.ts
+++ src/services/availability/service_provider_availability.service.ts
@@ -5,9 +5,8 @@ export class ServiceProviderAvailabilityService {
   constructor(private serviceProviderRepository: ServiceProviderRepository) {}
 
   public findAvailabilityAt(postalCode: string, date: Date): ServiceProviderEntity[] | null {
-    //How do you find the available service providers?
-    //Can you perhaps cache this?
-    //How can you
+    // How do you find the available service providers?
+    // Can you perhaps cache this? If so, how?
 
     return null;
   }
