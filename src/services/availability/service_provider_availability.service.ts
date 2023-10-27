88c8c181a669a15e763ae47f11628cc51b56e5ea
Caio Rosa <caio@seenons.com>
2023-10-27 12:35:38 +0200

    fix code requirements and add better description to Availbility Service

--- src/services/availability/service_provider_availability.service.ts
+++ src/services/availability/service_provider_availability.service.ts
@@ -1,13 +1,14 @@
 import { ServiceProviderRepository } from '../../providers/adapters/service_provider.repository';
-import { ServiceProviderEntity } from '../../providers/entities/service_provider.entity';
 
+/*
+  1. Implementation
+  - How do you find the available service providers?
+  - How to model correctly the response according to the expectation?
+*/
 export class ServiceProviderAvailabilityService {
   constructor(private serviceProviderRepository: ServiceProviderRepository) {}
 
-  public findAvailabilityAt(postalCode: string, date: Date): ServiceProviderEntity[] | null {
-    // How do you find the available service providers?
-    // Can you perhaps cache this? If so, how?
-
-    return null;
+  public findAvailabilityAt(postalCode: string, date: Date): any[] {
+    return [];
   }
 }
