aaad3d6191687decf000de489a7672c5d433394f
Caio Rosa <caio@seenons.com>
2024-02-06 17:12:58 +0100

    remove address from entities

--- src/domain/customer.ts
+++ src/domain/customer.ts
@@ -3,7 +3,6 @@ import { RegisteredWastePickup } from './registered_waste_pickup';
 export class Customer {
   id!: string;
   name!: string;
-  address!: string;
   postal_code!: string;
   registered_waste_pickups!: RegisteredWastePickup[];
 }
--- src/domain/service_provider.ts
+++ src/domain/service_provider.ts
@@ -3,6 +3,5 @@ import { ServiceProviderCoverage } from './service_provider_coverage';
 export class ServiceProvider {
   id!: string;
   name!: string;
-  address!: string;
   coverages!: ServiceProviderCoverage[];
 }
